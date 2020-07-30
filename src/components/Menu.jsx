d29bd34179752e25f17091d3ee3c13688ebf78a9
necheporenko <ne4eporenko.v@gmail.com>
2020-07-30 19:32:42 +0300

    update: mobile menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -12,7 +12,30 @@ const Menu = () => {
           <Link to="/about-us/">About us</Link>
         </li>
         <li className="menu-hasdropdown">
-          <Link to="/business-units/">Business Units</Link>
+          <a>Business Units</a>
+          <ul className="menu-dropdown">
+            <li>
+              <Link to="/power-generation/">Power Generation</Link>
+            </li>
+            <li>
+              <Link to="/power-substations/">Power Substations</Link>
+            </li>
+            <li>
+              <Link to="/oil-and-gas/">Oil & Gas</Link>
+            </li>
+            <li>
+              <Link to="/industry/">Industry</Link>
+            </li>
+            <li>
+              <Link to="/distribution-electrical-grids/">Distribution Electrical Grids</Link>
+            </li>
+            <li>
+              <Link to="/lines-of-transmission/">Lines of Transmission</Link>
+            </li>
+            <li>
+              <Link to="/lighting-systems/">Lighting Systems</Link>
+            </li>
+          </ul>
         </li>
         <li className="menu-hasdropdown">
           <a>Projects</a>
--- src/components/Mobile_menu.jsx
+++ src/components/Mobile_menu.jsx
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
-import { MDBNavbar, MDBNavbarNav, MDBNavItem, MDBCollapse, MDBHamburgerToggler } from 'mdbreact';
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavItem,
+  MDBCollapse,
+  MDBHamburgerToggler,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+} from 'mdbreact';
 
 class Mobilemenu extends Component {
   state = {
     collapse1: false,
     collapseID: '',
+  };
+
+  toggleCollapse = (collapseID) => () => {
+    this.setState((prevState) => ({
+      collapseID: prevState.collapseID !== collapseID ? collapseID : '',
+    }));
   };
 
   toggleSingleCollapse = (collapseId) => {
@@ -32,7 +48,41 @@ class Mobilemenu extends Component {
               </a>
             </MDBNavItem>
             <MDBNavItem>
-              <a className="mobile-link">Business Units</a>
+              <MDBDropdown>
+                <MDBDropdownToggle nav caret>
+                  <span>Business Units</span>
+                </MDBDropdownToggle>
+                <MDBDropdownMenu>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Aboutus_01'}>
+                    About Us 1
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Aboutus_02'}>
+                    About Us 2
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Services_01'}>
+                    Services 1
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Services_02'}>
+                    Services 2
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Our_expert'}>
+                    Our Expert
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Faq'}>FAQs</MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Contact_01'}>
+                    Contact Us 1
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Contact_02'}>
+                    Contact Us 2
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Error'}>
+                    Error Page
+                  </MDBDropdownItem>
+                  <MDBDropdownItem href={process.env.PUBLIC_URL + '/Element'}>
+                    Elements
+                  </MDBDropdownItem>
+                </MDBDropdownMenu>
+              </MDBDropdown>
             </MDBNavItem>
             <MDBNavItem>
               <a className="mobile-link">Projects</a>
