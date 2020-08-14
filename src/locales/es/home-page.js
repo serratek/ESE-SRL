0c5eaf010ffc86deda48b59aedd4e92974873e22
necheporenko <ne4eporenko.v@gmail.com>
2020-08-14 11:19:59 +0300

    update: es home page

--- src/locales/es/home-page.js
+++ src/locales/es/home-page.js
@@ -1,12 +1,12 @@
 const translation = {
-  'home.seoTitle': 'Home | We transform ideas into Energy',
-  'home.fsH5': 'Trust and Client Focus',
-  'home.fsH2': 'We transform ideas into <colored>Energy</colored>',
+  'home.seoTitle': 'Home | Transformamos Ideas en Energía',
+  'home.fsH5': 'Nos Enfocamos en el Cliente y en la Confianza',
+  'home.fsH2': 'Transformamos Ideas en <colored>Energía</colored>',
   'home.fsP': "Estamos orientados a satisfacer las necesidades de nuestros clientes y superar sus expectativas, a través de soluciones eficientes",
-  "home.2s.subtitle": "We Make Connections",
-  "home.2s.title": "We provide best business <colored>Units</colored>",
-  "home.4s.subtitle": "Our Blog",
-  "home.4s.title": "Check Out Our <colored>Latest News</colored>",
+  "home.2s.subtitle": "Hacemos conexiones",
+  "home.2s.title": "Proveemos las mejores <colored>Unidades</colored> de negocio",
+  "home.4s.subtitle": "Nuestras Noticias",
+  "home.4s.title": "Revise Nuestras <colored>Últimas Noticias</colored>",
   "home.5s.title": "Proyectos Recientes",
 };
 
