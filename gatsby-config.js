d009f19cc57ceb1e7b035bf6b2ecc35c87d0f161
necheporenko <ne4eporenko.v@gmail.com>
2020-08-15 12:43:41 +0300

    feat: optimize the website

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,31 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-lodash',
     'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-purgecss',
+      options: {
+        whitelistPatternsChildren: [
+          /^ti-/,
+          /^fa-/,
+          /^ttm-btn/,
+          /^ttm-icon/,
+          /^ttm-tabs/,
+          /^ttm-tab/,
+          /^c-timeline/,
+          /^flaticon/,
+        ],
+        purgeOnly: [
+          '/responsive.scss',
+          '/main.scss',
+          '/shortcode.scss',
+          '/bootstrap.min.css',
+          '/animate.css',
+          '/fontawesome.min.css',
+          '/slick.css',
+          '/slick-theme.css',
+        ],
+      },
+    },
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-catch-links', // This plugin intercepts all local links that have not been created in React using gatsby-link, and replaces their behavior with that of the gatsby-link navigate.
@@ -28,5 +53,10 @@ module.exports = {
         previews: false, // optional, default: true
       },
     },
+    // {
+    //   resolve: 'gatsby-plugin-webpack-bundle-analyser-v2',
+    //   options: {},
+    // },
+    'gatsby-plugin-no-javascript',
   ],
 };
